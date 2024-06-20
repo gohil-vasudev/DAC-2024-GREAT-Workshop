4588974ac7d30b9508c85f8dae166a0f99d2bbe7
Lucaz97 <lc4976@nyu.edu>
2024-06-20 17:28:02 -0400

    improved example

--- HLS/example.py
+++ HLS/example.py
@@ -22,3 +22,38 @@ completion = client.chat.completions.create(
         )
 
 print( completion.choices[0].message.content)
+
+# we need to keep the whole conversation if we want to keep prompting, including the responses we get:
+message_list.append(completion.choices[0].message)
+
+
+# the response contains both text and code, get the code:
+c_code_dut = completion.choices[0].message.content.split("```c")[1].split("```")[0]
+
+# we cam run gcc to check the code compiles
+
+file_name = f"temp_dut.c"
+with open(file_name, "w") as f:
+    # f.write(inlcudes)
+    f.write(c_code_dut)
+print("Compiling the code")
+
+import subprocess
+
+log = subprocess.run(["gcc", file_name, "-o", f"temp_dut"], capture_output=True)
+
+subprocess.run(["catapult", "-shell", "-file", tcl_file], capture_output=True)
+
+# To run catapult:
+# open reference tcl
+#read tcl
+with open("inputs/directives.tcl", "r") as f:
+    tcl = f.read()
+
+# format reference tcl with the top function and the c file to synthesize
+with open("my_tcl.tcl", "w") as f:
+    f.write(tcl.format(top_function="quicksort", c_file=file_name))
+
+# run catapult
+print("Running catapult")
+subprocess.run(["catapult", "-shell", "-file", "my_tcl.tcl"], capture_output=True)
